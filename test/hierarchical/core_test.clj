8e4216411940ed47cb868820847bfba744e44444
paul.rutledge <paul.rutledge@vodori.com>
2017-06-10 20:08:17 -0500

    add tests

--- test/hierarchical/core_test.clj
+++ test/hierarchical/core_test.clj
@@ -9,14 +9,19 @@
     (make-hierarchy)
     (derive ::pets ::animals)
     (derive ::farm ::animals)
+    (derive ::giraffe ::animals)
     (derive ::cows ::farm)
+    (derive ::pigs ::farm)
     (derive ::cats ::pets)
     (derive ::dogs ::pets)))
 
-
 (deftest hierarchical-map-lookups
-  (testing "An entry that exists directly in the map is always used."
+  (testing "Exact entries used first, then hierarchy, then not-founds."
     (let [hierarchy (get-hierarchy)
-          m {::dogs "Woof" ::pets "Love"}
+          m {::dogs "Woof" ::pets "Love" ::farm "Corn"}
           hiera (hierarchical hierarchy m)]
-      (is (= "Woof" (::dogs hiera))))))
+      (is (= "Woof" (::dogs hiera)))
+      (is (= "Love" (::cats hiera)))
+      (is (= "Corn" (::pigs hiera)))
+      (is (= "Corn" (::cows hiera "Some")))
+      (is (= "Some" (::giraffe hiera "Some"))))))
